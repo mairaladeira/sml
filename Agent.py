def2b2c5843f21b80924278b967f510090aa4341
Maira Machado Ladeira <mairamachadoladeira@gmail.com>
2015-05-29 17:59:05 +0200

    initialization of classes and methods

---
+++ Agent.py
@@ -0,0 +1,14 @@
+__author__ = 'Maira'
+
+
+class Agent:
+    def __init__(self, env, sizeEp, npMaxSteps):
+        self.env = env
+        self.sizeEp = sizeEp
+        self.maxStep = npMaxSteps
+
+    def decide(self):
+        return
+
+    def run(self):
+        return
--- Learning.py
+++ Learning.py
@@ -2,16 +2,62 @@ __author__ = 'Maira'
 
 
 class Model:
-    def __init__(self, m):
-        self.m = m
+    def __init__(self):
+        self.rule = []
+        self.ex = []
 
+    def addRule(self, rule):
+        self.rule = rule
+
+    def memorizeEx(self, ex):
+        self.ex = ex
+
+    def specialize(self, rule):
+        return
+
+    def getUncovEx(self, rule):
+        return
+
+    def contradicted(self, rule):
+        return True
+
+    def generalize(self, examples):
+        return
+
+    def IRALe(self, ex):
+        return
 
 class Rule:
-    def __init__(self, r):
-        self.r = r
+    def __init__(self, state, action, effect):
+        self.state = state
+        self.action = action
+        self.effect = effect
+        self.anc = None
 
+    def wellformed(self):
+        return True
+
+    def prematch(self, gstate, gaction, sigma):
+        return
+
+    def postmatch(self, gstate, geffect, sigma):
+        return
+
+    def covers(self, ex):
+        return
+
+    def postgeneralize(self, ex, ownSubst, exSubst):
+        return
 
 
 class Effect:
-    def __init__(self, e):
-        self.e = e
+    def __init__(self, pos, neg):
+        self.Add = pos
+        self.Del = neg
+
+    def getEffect(self, prevState, newState):
+        return
+
+    def filterOI(self, other, subst):
+        atoms = []
+        return atoms
